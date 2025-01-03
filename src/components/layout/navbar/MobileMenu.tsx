1e390fc5fd07740fbd23a63f2f8669913a90b95a
Aleks D <aleks.masayahin@gmail.com>
2025-01-04 01:10:57 +0800

    changes on nav components

--- src/components/layout/navbar/MobileMenu.tsx
+++ src/components/layout/navbar/MobileMenu.tsx
@@ -18,12 +18,12 @@ const MobileMenu = ({ menu }: { menu: MenuType[] }) => {
         aria-label='Open mobile menu'
         className='flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors md:hidden dark:border-neutral-700 dark:text-white'
       >
-        <MenuIcon className='h-4 w-4' />
+        <MenuIcon className='h-8 w-8' />
       </button>
 
       <Transition show={isOpen}>
         <Dialog onClose={toggleOpen} className='relative z-50'>
-          <Transition.Child
+          <TransitionChild
             as={Fragment}
             enter='transition-all ease-in-out duration-300'
             enterFrom='opacity-0 backdrop-blur-none'
@@ -33,49 +33,47 @@ const MobileMenu = ({ menu }: { menu: MenuType[] }) => {
             leaveTo='opacity-0 backdrop-blur-none'
           >
             <div className='fixed inset-0 bg-black/30' aria-hidden='true'></div>
-          </Transition.Child>
-          <Transition show={isOpen}>
-            <Dialog onClose={toggleOpen} className='relative z-50'>
-              <TransitionChild
-                as={Fragment}
-                enter='transition-all ease-in-out duration-300'
-                enterFrom='translate-x-[-100%]'
-                enterTo='translate-x-0'
-                leave='transition-all ease-in-out duration-200'
-                leaveFrom='translate-x-0'
-                leaveTo='translate-x-[-100%]'
-              >
-                <DialogPanel className='fixed bottom-0 left-0 right-0 top-0 flex h-full w-full flex-col bg-white pb-6 dark:bg-black'>
-                  <div className='p-4'>
-                    <button
-                      className='mb-4 flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white'
-                      onClick={toggleOpen}
-                      aria-label='Close mobile menu'
-                    >
-                      <X className='h-6' />
-                    </button>
-                    <div className='mb-4 w-full'>
-                      <SearchBar />
-                    </div>
-                    {menu.length > 0 ? (
-                      <ul className='flex w-full flex-col'>
-                        {menu.map((item: Menu) => (
-                          <li
-                            className='py-2 text-xl text-black transition-colors hover:text-neutral-500 dark:text-white'
-                            key={item.title}
-                          >
-                            <Link href={item.path} prefetch={true} onClick={toggleOpen}>
-                              {item.title}
-                            </Link>
-                          </li>
-                        ))}
-                      </ul>
-                    ) : null}
-                  </div>
-                </DialogPanel>
-              </TransitionChild>
-            </Dialog>
-          </Transition>
+          </TransitionChild>
+          <TransitionChild
+            as={Fragment}
+            enter='transition-all ease-in-out duration-300'
+            enterFrom='translate-x-[100%]' // Changed from -100% to 100%
+            enterTo='translate-x-0'
+            leave='transition-all ease-in-out duration-200'
+            leaveFrom='translate-x-0'
+            leaveTo='translate-x-[100%]' // Changed from -100% to 100%
+          >
+            <DialogPanel className='fixed bottom-0 right-0 top-0 flex h-full w-full sm:w-[70%] flex-col bg-white pb-6 dark:bg-black'>
+              <div className='p-4 flex flex-col'>
+                <div className='flex justify-end mb-4'>
+                  <button
+                    className='flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white'
+                    onClick={toggleOpen}
+                    aria-label='Close mobile menu'
+                  >
+                    <X className='h-8 w-8' />
+                  </button>
+                </div>
+                <div className='mb-4 w-full'>
+                  <SearchBar />
+                </div>
+                {menu.length > 0 ? (
+                  <ul className='flex w-full flex-col'>
+                    {menu.map((item: Menu) => (
+                      <li
+                        className='py-2 text-xl text-black transition-colors hover:text-neutral-500 dark:text-white'
+                        key={item.title}
+                      >
+                        <Link href={item.path} prefetch={true} onClick={toggleOpen}>
+                          {item.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                ) : null}
+              </div>
+            </DialogPanel>
+          </TransitionChild>
         </Dialog>
       </Transition>
     </>
--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -12,12 +12,9 @@ async function Navbar() {
 
   return (
     <nav className='flex items-center justify-between p-4 lg:px-6'>
-      <div className='block flex-none md:hidden'>
-        <MobileMenu menu={menu} />
-      </div>
       <div className='flex w-full items-center'>
         <div className='flex w-full md:w-1/3'>
-          <Link href={'/'} prefetch={true} className='mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6'>
+          <Link href={'/'} prefetch={true} className='mr-2 flex w-full items-center justify-start md:w-auto lg:mr-6'>
             <Logo />
             <div className='ml-2 flex-none text-sm font-medium uppercase md:hidden lg'>{siteName}</div>
           </Link>
@@ -40,9 +37,10 @@ async function Navbar() {
         <div className='hidden justify-center md:flex md:w-1/3'>
           <SearchBar />
         </div>
-        <div>
-          {/* <CartModal /> */}
-        </div>
+        <div>{/* <CartModal /> */}</div>
+      </div>
+      <div className='block flex-none md:hidden'>
+        <MobileMenu menu={menu} />
       </div>
     </nav>
   );
